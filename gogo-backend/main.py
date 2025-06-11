e42cfce2b8d79c5f809fe3736349cc305413f80d
leshiy <helg.leshiy@gmail.com>
2025-06-11 17:40:13 +0300

    add sample app and add endpoint for last info

--- gogo-backend/main.py
+++ gogo-backend/main.py
@@ -18,7 +18,7 @@ logger.info("starting server...")
 
 app = Flask("magav2")
 
-@app.route('/v1/player', methods=['POST'])
+@app.route('/v1/players/add', methods=['POST'])
 def flask_player():
     logger.info("flask request", extra={"tags": {"url": request.url}})
     req = request.get_json()
@@ -41,6 +41,38 @@ def flask_player():
     conn.close()
 
     return "", 200
+
+@app.route('/v1/players', methods=['GET'])
+def flask_player():
+    logger.info("flask request", extra={"tags": {"url": request.url}})
+
+    conn = psycopg2.connect(dbname='kakebo', user='whisper', password='2whisper2', host='91.105.196.201', port="5000")
+    players = []
+    with conn.cursor(cursor_factory = NamedTupleCursor) as curs:
+        curs.execute('''
+                    select t.id, t.nickname, t.lvl, t.class, t.stage, t.power, t.date
+                    from gogo_stats t
+                    inner join (
+                        select id, max(date) as MaxDate
+                        from gogo_stats
+                        group by id
+                    ) tm on t.id = tm.id and t.date = tm.MaxDate
+                    ''')
+        rows = curs.fetchall()
+        for row in rows:
+            players.append({
+                "id": row.id,
+                "nickname": row.nickname,
+                "lvl": row.lvl,
+                "cls": row.cls,
+                "stage": row.stage,
+                "power": row.power,
+                "update_date": row.date
+            })
+
+    conn.close()
+
+    return { "players": players }
 
 
 #app.run('0.0.0.0', port=8001, debug=True)
